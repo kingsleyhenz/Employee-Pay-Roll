2c6aa780137f8323fadbd12dbb5f9ab065d23275
Kingsley Henshaw <ayehenz29@gmail.com>
2022-07-31 20:53:00 +0100

    first commit

--- src/main/PayRollDir.java
+++ src/main/PayRollDir.java
@@ -1,12 +1,13 @@
 package main;
 import java.*;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 
 public class PayRollDir {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         try{
             Path empPay = Paths.get("EmployeePayRoll");
            if (Files.exists(empPay)) {
@@ -18,5 +19,9 @@ public class PayRollDir {
         }catch (Exception e){
                 e.printStackTrace();
         }
+
+        Path java = Paths.get("java");
+        Path directories = Files.createDirectories(java);
     }
+
 }
